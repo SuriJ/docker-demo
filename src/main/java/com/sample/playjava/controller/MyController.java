9006e197c9bca58038fa30628cffbf9eb224fa83
Suresh Jampana <suresh.list@gmail.com>
2021-07-17 21:25:35 +0530

    Initial commit for Spring boot Docker Kubernetes

---
+++ src/main/java/com/sample/playjava/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.sample.playjava.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/test-docker")
+	public String getData() {
+		return "In Docker tutorial project";
+	}
+
+}
